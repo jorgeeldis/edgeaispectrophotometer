9fba1f482659b7444a3b64a9d0df7ceb91312af3
Your Personal Name <jorgeeldisg30@gmail.com>
2026-07-30 20:41:27 -0500

    Update loop to initialize 'begin' and 'end' variables with a small non-zero value for improved calculations

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -85,7 +85,7 @@ void loop()
 
   int channelsRead = mySensor.getData(myData);
 
-  float begin = 0;
+  float begin = 0.0001;
   float F1 =  mySensor.getChannelData(CH_PURPLE_F1_405NM);
   float F2 =  mySensor.getChannelData(CH_DARK_BLUE_F2_425NM);
   float FZ =  mySensor.getChannelData(CH_BLUE_FZ_450NM);
@@ -98,7 +98,7 @@ void loop()
   float F7 =  mySensor.getChannelData(CH_RED_F7_690NM);
   float F8 =  mySensor.getChannelData(CH_DARK_RED_F8_745NM);
   float NIR =  mySensor.getChannelData(CH_NIR_855NM);
-  float end = 0;
+  float end = 0.0001;
 
 
   Serial.println("\n--- Spectral Readings ---");

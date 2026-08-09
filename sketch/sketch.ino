52c9b40e3dc56a572e6d2f5b946897c73882a810
Your Personal Name <jorgeeldisg30@gmail.com>
2026-08-09 14:16:42 -0500

    Fix syntax error in loop function for data transmission to Python

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -132,8 +132,7 @@ void loop()
     Bridge.notify(
         "record_sensor_samples", values[0], values[1], values[2], values[3],
         values[4], values[5], values[6], values[7],
-        values[8], values[9], values[10], values[11],
-        );
+        values[8], values[9], values[10], values[11]);
 
     Serial.println("Sent to Python!");
     Serial.println("--------------------------------");
